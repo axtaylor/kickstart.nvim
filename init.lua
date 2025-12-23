12474a760973cfe004172749f342180b37443bb8
axtaylor <208505619+axtaylor@users.noreply.github.com>
2025-12-23 00:46:09 -0500

    some setup testing

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -205,6 +205,17 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+
+
+
+vim.opt.lazyredraw = true
+vim.opt.ttyfast = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.synmaxcol = 300
+vim.opt.cursorline = false  -- Major improvement on 4K
+vim.opt.list = false
+vim.opt.wrap = false
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -894,7 +905,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
@@ -940,11 +951,12 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'python', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -973,7 +985,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
@@ -984,7 +996,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,188 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+
+
+
+
+
+    {
+        'projekt0n/github-nvim-theme',
+        -- priority = 1000,
+        plugins,
+        config = function()
+
+        ---@diagnostic
+        require('github-theme').setup {
+            styles = {
+                comments = { italic = false },
+            },
+        }
+        -- vim.cmd.colorscheme 'github_dark_dimmed'
+        end,
+    },
+
+    {
+        'olimorris/onedarkpro.nvim',
+        -- priority = 1000,
+        config = function()
+        require('onedarkpro').setup({
+            transparent = true,
+        })
+        vim.cmd.colorscheme 'onedark'
+        end,
+    },
+
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        config = function()
+        require('catppuccin').setup({
+
+            transparent = true,
+        })
+        -- vim.cmd.colorscheme 'catppuccin-latte'
+        end,
+    },
+
+
+
+
+
+
+    require 'kickstart.plugins.gitsigns',
+    require 'kickstart.plugins.neo-tree',
+
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+
+        config = function()
+
+        require("neo-tree").setup({
+
+            filesystem = {
+
+                filtered_items = {
+                    visible = true,  -- Show all files, including ignored ones
+                    hide_gitignored = false,  -- Don't hide git-ignored files
+                    -- follow_current_file = true,  -- Follow the current file in the tree
+
+                },
+
+                git_status = {
+                    show_ignored = true,  -- Show git-ignored files and directories
+                    show_untracked = true,  -- Show untracked files
+                    show_modified = true,  -- Show modified files
+                    show_staged = true,  -- Show staged files
+
+                },
+
+                window = {
+                    width = 35,  -- Width of Neo-tree window
+                    position = "left",  -- Position of the window (left or right)
+                }
+            },
+        })
+        end,
+    },
+
+
+
+
+
+
+    {
+         'nvim-lualine/lualine.nvim',
+         dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    },
+
+
+
+
+
+
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+
+            sidebar_filetypes = {
+
+                NvimTree = true,
+                ['neo-tree'] = {event = 'BufWipeout'},
+
+            },
+
+        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+        -- animation = true,
+        -- insert_at_start = true,
+        -- …etc.
+        },
+
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
+
+
+
+
+    { 
+        'nvimtools/none-ls.nvim',
+        dependencies = {
+            'nvimtools/none-ls-extras.nvim',
+            'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+        }
+    },
+
+
+
+
+
+
+    {
+        "karb94/neoscroll.nvim",
+        opts = {},
+
+
+        config = function()
+        require('neoscroll').setup({
+            hide_cursor = true,
+        })
+
+       local neoscroll = require('neoscroll')
+        
+        -- Mouse scroll mapping
+            vim.keymap.set({'n', 'v', 'x'}, '<ScrollWheelUp>', function()
+                neoscroll.scroll(-15, { hide_cursor=true, duration = 225,})
+            end)
+
+
+            vim.keymap.set({'n', 'v', 'x'}, '<ScrollWheelDown>', function()
+                neoscroll.scroll(15, { hide_cursor=true, duration = 225,})
+            end)
+
+        end,
+    },
+
+    
+
+
+
+
+
+
+
+}
